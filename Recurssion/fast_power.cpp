1915018ca3d691135480b521bdf6680e91ed36d5
priyashu-code <priyanshu.28bisht@gmail.com>
2025-07-18 17:37:33 +0530

    faster way to find power in log n time

--- Recurssion/fast_power.cpp
+++ Recurssion/fast_power.cpp
@@ -1,8 +1,8 @@
 /*
   So to decrease the time complexity we can change the recurence
 
-  a^n = (a^(n/2))^2 if even  2^18= (2^9)^2
-  a^n = a*(a^(n/2))^2 if odd 2^19= 2*(2^9)^2
+  a^n = (a^(n/2))^2 if power is even  2^18= (2^9)^2
+  a^n = a*(a^(n/2))^2 if power is odd 2^19= 2*(2^9)^2
 
   So instead of O(N) it will be O(log N) as no. of steps will decrease
   */
